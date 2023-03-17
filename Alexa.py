718a06858f721b10113f5c633d6c0f670802070a
Spooder-NiTin <127127169+nithin264200@users.noreply.github.com>
2023-03-17 12:45:45 +0530

    Add files via upload

---
+++ Alexa.py
@@ -0,0 +1,54 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+listener=sr.Recognizer()
+engine=pyttsx3.init()
+voices=engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('cheppu ra huka...')
+            voice=listener.listen(source)
+            command=listener.recognize_google(voice)
+            command=command.lower()
+            if 'alexa' in command:
+                command=command.replace('alexa','')
+                print(command)
+    except:
+        pass
+    return command
+def run_alexa():
+    command=take_command()
+    print(command)
+    if 'play' in command:
+        command=command.replace('play','')
+        talk('playing'+command)
+        pywhatkit.playonyt(command)
+    elif 'love' in command:
+        print(talk('No'))
+    elif 'time' in command:
+        time=datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        talk('current time is'+ time)
+    elif 'who' in command:
+        person=command.replace('who','')
+        info=wikipedia.summary(person,1)
+        print(info)
+        talk(info)
+    elif 'joke' in command:
+        jokes=pyjokes.get_joke()
+        print(jokes)
+        talk(jokes)
+    else:
+        print(talk('Please say it again!'))
+while True:
+    run_alexa()
+
+
